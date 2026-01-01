bfe17184a91d50a672b3aa6e5fccfe018ba7edc6
Dhruv Mohan Shukla <127665916+dhruvmohan867@users.noreply.github.com>
2026-01-01 17:04:15 +0530

    Add Thymeleaf dependency and create dashboard template

--- src/main/java/com/dhruv/taskmanager/controller/ViewController.java
+++ src/main/java/com/dhruv/taskmanager/controller/ViewController.java
@@ -1,5 +1,12 @@
 package com.dhruv.taskmanager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ViewController {
-    
+    @GetMapping("/")
+    public String dashboard() {
+        return "dashboard"; // resolves src/main/resources/templates/dashboard.html
+    }
 }
